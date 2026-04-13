396f0aa7bd08c59f5ab352e8dacd19ee9efb7dcf
luongvantrang <luongvantrang2020@gmail.com>
2026-04-13 20:49:30 +0700

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,17 +1,23 @@
 import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
-  const { userid, key } = req.query;
-  const SECRET_KEY = process.env.SECRET_KEY;
+  try {
+    const { userid, key } = req.query;
+    const SECRET_KEY = process.env.SECRET_KEY;
 
-  if (!userid || !key) {
-    return res.status(400).send('Thiếu thông tin');
+    if (!userid || !key) {
+      return res.status(400).send('Thiếu UserID hoặc Key');
+    }
+
+    if (key !== SECRET_KEY) {
+      return res.status(403).send('Key không đúng!');
+    }
+
+    await kv.sadd('whitelist', userid.toString());
+    
+    return res.status(200).send('Thành công! Bạn đã được kích hoạt key.');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Lỗi server');
   }
-
-  if (key !== SECRET_KEY) {
-    return res.status(403).send('Key không đúng!');
-  }
-
-  await kv.sadd('whitelist', userid);
-  return res.status(200).send('Key hợp lệ! Bạn có thể dùng script.');
 }
